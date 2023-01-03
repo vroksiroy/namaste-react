7c7dfa75aad3c787c98fdca0d11727b7c1d2fcee
Vroksi Roy <royvroksi@gmail.com>
2023-01-03 20:20:27 +0530

    First Program Using React and Javascript

---
+++ App.js
@@ -0,0 +1,17 @@
+// First Program Using Javascript
+/*
+    const heading = document.createElement('h1')
+    heading.innerHTML = "Namaste everyone from javascript!"
+
+    const root = document.getElementById('root')
+    root.appendChild(heading)
+ */
+
+// First Program Using React
+const heading1 = React.createElement('h1', {}, 'Heading1')
+const heading2 = React.createElement('h2', {}, 'Heading2')
+
+const container = React.createElement('div', { id: 'container' }, [heading1, heading2])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(container)
